0a4717dfd889d28f59e23dd8555c87d4d0aba023
romarioL <45340806+romarioL@users.noreply.github.com>
2019-04-29 10:53:50 -0300

    Add files via upload

---
+++ FreelancerX/index.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<div class="banner-folders">
+	<div class="folder">
+		<img class="folder-img" src="<?php bloginfo('template_url'); ?>/img/html.svg" />
+		<h4>HTML</h4>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+
+	<div class="folder">
+		<img class="folder-img" src="<?php bloginfo('template_url'); ?>/img/javascript.png" />
+		<h4>Javascript</h4>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+
+	<div class="folder">
+		<img class="folder-img" src="<?php bloginfo('template_url'); ?>/img/php-logo.svg" />
+		<h4>PHP</h4>
+		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+	</div>
+
+</div>
+
+
+<?php  get_footer(); ?>
