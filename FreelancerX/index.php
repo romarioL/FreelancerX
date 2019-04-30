1531876a29983ebebe99b037e367570de1bf34c4
romarioL <45340806+romarioL@users.noreply.github.com>
2019-04-30 16:35:59 -0300

    Add files via upload

--- FreelancerX/index.php
+++ FreelancerX/index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<div class="banner-folders">
+<div class="banner-folders ">
 	<div class="folder">
 		<img class="folder-img" src="<?php bloginfo('template_url'); ?>/img/html.svg" />
 		<h4>HTML</h4>
@@ -19,6 +19,18 @@
 		<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
 	</div>
 
+</div>
+
+<div class="banner-posts">
+	<div class="row">
+		<?php  if(have_posts()): while(have_posts()):  the_post()?>
+		<div class="col s12 l4 post">
+			<?php the_post_thumbnail(); ?>
+			<a href="<?php  the_permalink(); ?>"  class="post-link"><h4><?php  the_title(); ?></h4></a>
+			<p><?php  the_excerpt();?></p>
+		</div>
+	<?php endwhile; endif; ?>
+	</div>
 </div>
 
 
