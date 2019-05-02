2a5ab8dc022e11bc57dd3237d730332e9c8fe182
romarioL <45340806+romarioL@users.noreply.github.com>
2019-05-02 10:54:18 -0300

    Add files via upload

--- FreelancerX/footer.php
+++ FreelancerX/footer.php
@@ -1,5 +1,13 @@
 <!--JavaScript at end of body for optimized loading-->
 
+<footer class="page-footer black">
+	<div class="footer-copyright">
+            <div class="container">
+            © 2019 Copyright Text
+            </div>
+          </div>
+</footer>
+
 <?php  wp_footer();?>
       <script type="text/javascript" src="<?php  bloginfo('template_url');?>/js/materialize.min.js"></script>
     </body>
